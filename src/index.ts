bf3ac3720ed1e4515cf45c990eefbd4380ae7066
nzhming <1078830088@qq.com>
2022-06-07 14:05:54 +0800

    learn generic

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,16 @@
-const author: string = 'nizhm';
-console.log(author);
+function generic<T>(arg: T): T {
+  console.log(arg);
+  console.log(typeof arg);
+  return arg;
+}
+
+type Person = {
+  name: string,
+  age: number
+}
+
+const person: Person = {
+  name: 'nizhm',
+  age: 25
+}
+generic(person);
